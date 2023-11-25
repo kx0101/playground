cd0e2fbdf4629be068dbca1ea500d0a8163b82db
Elijah Koulaxis <liakos.koulaxis@yahoo.com>
2023-11-25 23:09:02 +0200

    asdfasdf

--- Program.cs
+++ Program.cs
@@ -1,24 +1,57 @@
-using System;
-
 namespace letssee
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string post = "Hello how are you doing damn this is quite a long text isnt it hahah blah blah blah";
-            var shortenedPost = post.Shorten(3);
+            Solution s = new Solution();
 
-            IEnumerable<int> numbers = new List<int>() { 1, 5, 10, 18, 22 };
+            Print(s.FizzBuzz(15));
+        }
 
-            foreach (int num in numbers)
+        static void Print(IList<string> list)
+        {
+            foreach (var item in list)
             {
-                Console.WriteLine(num);
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            Dictionary<int, string> map = new Dictionary<int, string>
+            {
+                { 3, "Fizz" },
+                { 5, "Buzz" },
+            };
+
+            List<string> result = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string output = "";
+                foreach (var entry in map)
+                {
+
+                    if (i % entry.Key == 0)
+                    {
+                        output += entry.Value;
+                    }
+                }
+
+                if (output == "")
+                {
+                    output += i.ToString();
+                }
+
+                result.Add(output);
+
             }
 
-            Console.WriteLine(numbers.Max());
-
-            Console.WriteLine("takis");
+            return result;
         }
     }
 }
