eb64d6061e95b4a0f82a18b1ed4bb1b6a7110d18
kx0101 <liakos.koulaxis@yahoo.com>
2023-12-27 13:22:20 +0200

    asdfsaf

--- Program.cs
+++ Program.cs
@@ -4,28 +4,48 @@ namespace letssee
     {
         static void Main(string[] args)
         {
-            int fib = 9;
-            Dictionary<int, int> fibDict = new Dictionary<int, int>();
-            fibDict.Add(0, 0);
-            fibDict.Add(1, 1);
+            List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> list2 = new List<int> { 1, 3, 5, 6, 8 };
 
-            Console.WriteLine("Fibonacci of {0} is {1}", fib, Fibonacci(fib, fibDict));
+            var result = Intersect(list1, list2);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-        static int Fibonacci(int n, Dictionary<int, int> fibDict)
+        // This is the optimal approach - O(n) and O(1) space excluding the result list ofc
+        static List<int> Intersect(List<int> list1, List<int> list2)
         {
-            if (fibDict.ContainsKey(n))
+            List<int> result = new List<int>();
+            HashSet<int> set = new HashSet<int>(list1);
+
+            foreach (var item in list2)
             {
-                return fibDict[n];
+                if (set.Contains(item))
+                {
+                    result.Add(item);
+                }
             }
 
-            for (int i = 2; i <= n; i++)
-            {
-                if (!fibDict.ContainsKey(i))
-                    fibDict.Add(i, fibDict[i - 1] + fibDict[i - 2]);
-            }
-
-            return fibDict[n];
+            return result;
         }
+
+        // This is the naive approach - O(n^2)
+        /* static List<int> Intersect(List<int> list1, List<int> list2) */
+        /* { */
+        /*     List<int> result = new List<int>(); */
+        /*  */
+        /*     foreach (var item in list1) */
+        /*     { */
+        /*         if (list2.Contains(item)) */
+        /*         { */
+        /*             result.Add(item); */
+        /*         } */
+        /*     } */
+        /*  */
+        /*     return result; */
+        /* } */
     }
 }
